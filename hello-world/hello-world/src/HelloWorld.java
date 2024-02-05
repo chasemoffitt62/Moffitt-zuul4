bab58a23f74e9bf734a9e35d15640e221a54af00
chasemoffitt62 <79743067+chasemoffitt62@users.noreply.github.com>
2024-02-04 16:19:51 -0800

    Finished all additions for Branch
    Completed commits ready for pull request.

---
+++ hello-world/hello-world/src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+/**
+ * A class which displays a Hello, World! message
+ *
+ * @author Chase Moffitt
+ * @version 04.2.2024
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println(new Date());
+    }
+}
